fea5eb7769715382bcec1f78ba97cc6b465ab560
User <leeseongmin451@gmail.com>
2022-07-12 14:53:50 +0900

    mycommit

---
+++ Assets/Script/Scripts_lsm/PopupWindow.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopupWindow : MonoBehaviour
+{
+    [SerializeField]
+    private PopupWindowType type;
+
+    enum PopupWindowType
+    {
+        
+    }
+}
